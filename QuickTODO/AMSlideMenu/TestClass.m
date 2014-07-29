8a3ceea1737d58a3404eec1da13f5b041b2e1ff2
Daniel Munoz <danmunoz@me.com>
2014-07-29 10:51:31 -0400

    test class added

---
+++ QuickTODO/AMSlideMenu/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  QuickTODO
+//
+//  Created by Daniel Munoz on 7/29/14.
+//  Copyright (c) 2014 GreenCraft. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+//This is a class to test revert merge
+@end
